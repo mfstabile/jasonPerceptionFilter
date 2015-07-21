ad3e889e5ffbeaf4d939205470862224b8f9e51c
mfstabile <MarcioNote@MarcioJr-Note.local>
2015-07-21 15:33:21 -0300

    Merge

--- XMLFilter/Jason-1.4.1/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home'
-#cd jason-svn
-ant clean
-ant jar
-ant plugin
-ant apidoc
-bash bin/jason.sh
--- XMLFilter/Jason-1.4.1/run8.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#cd jason-svn
-bash bin/jason.sh
